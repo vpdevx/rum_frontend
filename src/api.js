cf52707a7400bc5a606f6ec06eed0d3710bd723c
Vinicius Carvalho Pires <vinicpires03@gmail.com>
2024-11-13 14:14:40 -0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -38,7 +38,7 @@ export const fetchCustomers = async () => {
   
   export const addProduct = async (product) => {
     try {
-      const response = await fetch('https://api-rum-teste.k8s.lab4ever.xyz/product/add', {
+      const response = await fetch('https://api-rum-teste.k8slab4ever.xyz/product/add', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
